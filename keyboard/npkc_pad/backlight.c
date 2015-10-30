9ce948a62976fd36f5b2f897950cba108ae15c06
Yang Liu <i@yangliu.name>
2015-10-30 07:47:00 -0700

    test the debug output function

--- keyboard/npkc_pad/backlight.c
+++ keyboard/npkc_pad/backlight.c
@@ -81,7 +81,7 @@ void TLC5940_RefreshGSData(void) {
 
 void backlight_set(uint8_t level){
   current_level = level;
-  dprintf("Backlight Set, level: %d", level);
+  print("Backlight Set, level: "); print_dec(level); print("\n");
   if (level>0) {
     TLC5940_Init();
     TLC5940_Enable();
